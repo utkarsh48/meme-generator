984b40ee76739b00356703f8391310789a9ee117
mridul-dhiman <devilshouse9076@gmail.com>
2020-05-21 21:50:41 +0530

    fixed stroke bug

--- canvas.js
+++ canvas.js
@@ -96,7 +96,7 @@ class Texter{
         context.font=`${this.size}px impact`;
         context.fillStyle=this.fColor;
         context.strokeStyle=this.oColor;
-        context.lineWidth=2;
+        context.lineWidth=this.stroke;
         context.textAlign='center';
         context.fillText(`${this.text}`, this.x,this.y);
         context.strokeText(`${this.text}`, this.x,this.y);
@@ -243,9 +243,3 @@ window.onload=function(){
 }
 
 
-//saviour
-saveBtn1.addEventListener('click',()=>{
-    //iframes can open base64urls
-    var win = window.open();
-    win.document.write('<iframe src="' + c.toDataURL("image/png") + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
-});
